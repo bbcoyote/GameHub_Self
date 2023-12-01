6c8f55c8a12171444deb872c389451588c9f4d50
Allen Huffman <huffmancharles14@gmail.com>
2023-12-01 16:18:15 -0600

    add chakra ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import "./App.css";
+import { Button, ButtonGroup } from "@chakra-ui/react";
 
 function App() {
   return (
     <>
       <h1>Hello, World</h1>
+      <ButtonGroup>
+        <Button bg='orange'> Click Me </Button>
+      </ButtonGroup>
     </>
   );
 }
